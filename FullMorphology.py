251130391c7d2eb705e52004396f9c1dc7196d7e
TristanAgaesse <TristanAgaesse@users.noreply.github.com>
2014-12-17 17:21:35 +0100

    Commit CEA version
    Some cosmetic changes to FullMorphology and
    PNMWatershedAndGeometryAnalysis.
    Change distance map to euclidian. slightly better memory management

--- FullMorphology.py
+++ FullMorphology.py
@@ -9,17 +9,55 @@ import numpy as np
 from skimage import morphology
 from scipy import ndimage
 import sys
+import os
 
-foldername ='/home/270.12-Modeling_PEMFC_Li/theseTristan/PSI_drainage_python/'
-sys.path.append(foldername)
-
+sys.path.append(os.getcwd())
 import tifffile as tff
 
-def main(inputFileName,outputFilename,voxelLength):
-        
-    #inputFileName = foldername+"PSI_sampleDrainage_2540.tif"
+
+#----------------------------------------------------------------------------------------------
+
+def FullMorphologyHydrophobicStep(distanceMap,capillaryLength,inletVoxels):
     
-    myImg=tff.imread(inputFileName)
+    #Find the centers of the water balls
+    indicesCenters=(distanceMap>capillaryLength).reshape(distanceMap.shape)
+    invadedVoxels=np.zeros(distanceMap.shape,dtype=bool)
+    invadedVoxels[indicesCenters]=True
+    del indicesCenters
+    
+    
+    #Dilate with a ball of size capillaryLength
+    ball=morphology.ball(capillaryLength)
+    invadedVoxels=ndimage.morphology.binary_dilation(invadedVoxels, structure=ball)     
+      
+              
+    #Keep only water connected with the inlet          
+    structuringElement = np.ones((3,3,3))
+    invadedVoxels=ndimage.measurements.label(invadedVoxels,structuringElement)[0]
+    
+    labelsToKeep=np.unique(invadedVoxels[inletVoxels])    
+    labelsToKeep=labelsToKeep[labelsToKeep>0]
+    
+    foo=np.zeros(invadedVoxels.shape,dtype=bool)
+    
+    for iLabel in labelsToKeep :
+        foo[invadedVoxels==iLabel] = True
+    
+    del labelsToKeep
+    
+    invadedVoxels=invadedVoxels.astype(np.bool)
+    invadedVoxels[np.logical_not(foo)]=False
+    invadedVoxels[foo]=True
+    
+    
+    return invadedVoxels
+    
+
+#----------------------------------------------------------------------------------------------
+
+def main(inputFileName,outputFileName,voxelLength):
+            
+    myImg=tff.imread(inputFileName).astype(np.uint8)
     
     inletVoxels = np.zeros(myImg.shape,dtype=bool)
     inletVoxels[-1,:,:] = True
@@ -35,68 +73,25 @@ def main(inputFileName,outputFilename,voxelLength):
 #
 #    return saturation
     
-    #voxelLength=(2.2e-6)*2540/float(2540)
+
     gamma=72e-3
     
+    #Calculs de répartition de l'eau pour 53 mbar, 39 mbar, 28 mbar, 22 mbar, 14 mbar 
+    #pressureList=[5300,3900,2800,2200,1400]
+    pressureList=[2200,1400]
     
-    pressure = 53*100
-    water = FullMorphologyHydrophobicStep(distanceMap,2*gamma/(pressure*voxelLength),inletVoxels)
-    myImg[water] = 153
+    for i in range(len(pressureList)):
+        print('begin '+str(i))
+        pressure = pressureList[i]
+        water = FullMorphologyHydrophobicStep(distanceMap,2*gamma/(pressure*voxelLength),inletVoxels)
+        myImg[water] = int(100+pressure/100)
+        del water  
     
-    pressure = 39*100
-    water = FullMorphologyHydrophobicStep(distanceMap,2*gamma/(pressure*voxelLength),inletVoxels)
-    myImg[water] = 139
-    
-    pressure = 28*100
-    water = FullMorphologyHydrophobicStep(distanceMap,2*gamma/(pressure*voxelLength),inletVoxels)
-    myImg[water] = 128
-    
-    pressure = 22*100
-    water = FullMorphologyHydrophobicStep(distanceMap,2*gamma/(pressure*voxelLength),inletVoxels)
-    myImg[water] = 122
-    
-    pressure = 14*100
-    water = FullMorphologyHydrophobicStep(distanceMap,2*gamma/(pressure*voxelLength),inletVoxels)
-    myImg[water] = 114
-    
-    tff.imsave(outputFileName,myImg)
-    
-
-#----------------------------------------------------------------------------------------------
-
-def FullMorphologyHydrophobicStep(distanceMap,capillaryLength,inletVoxels):
-    
-    indicesInvaded=(distanceMap>capillaryLength).reshape(distanceMap.shape)
-    invadedVoxels=np.zeros(distanceMap.shape,dtype=bool)
-    invadedVoxels[indicesInvaded]=True
-    
-    
-    ball=morphology.ball(capillaryLength)
-    #invadedVoxels=mahotas.morph.dilate(invadedVoxels,ball)
-    invadedVoxels=morphology.binary_dilation(invadedVoxels, selem=ball)    
-              
-              
-    structuringElement = np.ones((3,3,3))
-    invadedVoxels=ndimage.measurements.label(invadedVoxels,structuringElement)[0]
-    
-    labelsToKeep=np.unique(invadedVoxels[inletVoxels])    
-    labelsToKeep=labelsToKeep[labelsToKeep>0]
-    
-    foo=np.zeros(invadedVoxels.shape,dtype=bool)
-    
-    for iLabel in labelsToKeep :
-        foo[invadedVoxels==iLabel] = True
-    
-    invadedVoxels=invadedVoxels.astype(np.bool)
-    invadedVoxels[np.logical_not(foo)]=False
-    invadedVoxels[foo]=True
-    
-    
-    return invadedVoxels
+      
+    tff.imsave(outputFileName,myImg.astype(np.uint8))
     
 
 
-    
 #----------------------------------------------------------------------------------------------
     
 if __name__ == "__main__":
