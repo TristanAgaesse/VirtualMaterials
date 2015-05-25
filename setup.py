ce63c51f8ec3e88d3405dfaf20d24ab2be373275
GreenToto <tristanagaesse@users.noreply.github.com>
2015-05-25 11:45:29 +0200

    Add setup.py script

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+sys.path.append(os.getcwd())
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='VirtualMaterials',
+    packages=['OpenPNM',
+              'OpenPNM.Base',
+              'OpenPNM.Network',
+              'OpenPNM.Network.models',
+              'OpenPNM.Geometry',
+              'OpenPNM.Geometry.models',
+              'OpenPNM.Phases',
+              'OpenPNM.Phases.models',
+              'OpenPNM.Physics',
+              'OpenPNM.Physics.models',
+              'OpenPNM.Utilities',
+              'OpenPNM.Algorithms',
+              'OpenPNM.Postprocessing'],
+    version='1.1',
+    description="A framework for conducting pore network modeling simulations of multiphase transport in porous materials.",
+    author='OpenPNM Team',
+    author_email='jeff.gostick@mcgill.ca',
+    download_url='https://github.com/pmeal/OpenPNM/',
+    url='https://github.com/pmeal/OpenPNM'
+)
