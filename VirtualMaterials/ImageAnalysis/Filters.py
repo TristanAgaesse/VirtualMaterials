7698b74cbee7a3004f3acc5426f43ac3b160ff19
TristanAgaesse <TristanAgaesse@users.noreply.github.com>
2016-08-26 15:11:55 +0200

    new pvFilters : vmat.Morphology and vmat.Filters

--- VirtualMaterials/ImageAnalysis/Filters.py
+++ VirtualMaterials/ImageAnalysis/Filters.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import SimpleITK as sitk
 from VirtualMaterials.Utilities import Utilities as utils  
+import numpy as np
 #Apply filters to the image using the SimpleITK library
 
 #---------------------------------------------------
@@ -42,7 +43,10 @@ def Threshold(image, lower=0.0,upper=1.0,outsideValue=0.0):
     memoryType = utils.BestMemoryType(image.max())    
     itkimage = sitk.GetImageFromArray(image.astype(memoryType))
 
-    itkimage = sitk.Threshold(image, lower=lower,upper=upper,outsideValue=outsideValue)
+    itkimage = sitk.Threshold(itkimage, 
+                              lower=lower,
+                              upper=upper,
+                              outsideValue=outsideValue)
     
     image=sitk.GetArrayFromImage(itkimage).astype(memoryType) 
     return image
@@ -50,7 +54,6 @@ def Threshold(image, lower=0.0,upper=1.0,outsideValue=0.0):
 
 #---------------------------------------------------
 def OtsuThreshold(image,
-               maskImage,
                insideValue = 100,
                outsideValue = 0,
                numberOfHistogramBins = 128,
@@ -58,7 +61,6 @@ def OtsuThreshold(image,
                maskValue = 255   ):
     """OtsuThreshold : see SimpleITK.OtsuThreshold
     :param image : numpy image
-    :param maskImage: 
     :param insideValue: 
     :param outsideValue: 
     :param numberOfHistogramBins: 
@@ -69,7 +71,6 @@ def OtsuThreshold(image,
     :Example:    
     import VirtualMaterials as vmat
     filteredImage = vmat.ImageAnalysis.Filters.OtsuThreshold(image,
-               maskImage,
                insideValue = 100,
                outsideValue = 0,
                numberOfHistogramBins = 128,
@@ -79,25 +80,24 @@ def OtsuThreshold(image,
     
     
     assert(image.min()>=0)
-    memoryType = utils.BestMemoryType(image.max())    
+    memoryType = memoryType = utils.BestMemoryType(image.max())    
     itkimage = sitk.GetImageFromArray(image.astype(memoryType))
-    
+       
     #OTSU Threshold FILTER
-    itkimage= sitk.OtsuThreshold(itkimage,
-                       maskImage,
-                       insideValue = 100,
-                       outsideValue = 0,
-                       numberOfHistogramBins = 128,
-                       maskOutput = True,
-                       maskValue = 255 
-                       )		
+    itkimage= sitk.OtsuThreshold(
+                       itkimage,
+                       np.uint8(insideValue),
+                       np.uint8(outsideValue),
+                       np.uint32(numberOfHistogramBins),
+                       bool(maskOutput),
+                       np.uint8(maskValue) 
+                       )
                        
-
     image=sitk.GetArrayFromImage(itkimage).astype(memoryType) 
     return image
                        
 #---------------------------------------------------
-def AnisotropicDiffusionFilter(image,
+def AnisotropicDiffusionFilter_Gradient(image,
                           timeStep = 0.125,
                           conductanceParameter=3.0,
                           conductanceScalingUpdateInterval=1,
@@ -112,26 +112,69 @@ def AnisotropicDiffusionFilter(image,
     
     :Example:    
     import VirtualMaterials as vmat
-    filteredImage = vmat.ImageAnalysis.Filters.AnisotropicDiffusionFilter(image,
+    filteredImage = vmat.ImageAnalysis.Filters.AnisotropicDiffusionFilter_Gradient(image,
                           timeStep = 0.125,
                           conductanceParameter=3.0,
                           conductanceScalingUpdateInterval=1,
                           numberOfIterations = 20 )
     """          
-
-    assert(image.min()>=0)
-    memoryType = utils.BestMemoryType(image.max())    
-    itkimage = sitk.GetImageFromArray(image.astype(memoryType))
     
-    itkimage = sitk.GradientAnisotropicDiffusion(itkimage,
-                                      timeStep = 0.125,
-                                      conductanceParameter=3.0,
-                                      conductanceScalingUpdateInterval=1,
-                                      numberOfIterations = 20
-                                      )
+    #https://arxiv.org/pdf/1503.00992.pdf
+    assert(image.min()>=0)
+    itkimage = sitk.GetImageFromArray(image.astype(np.uint8))
+    
+    caster = sitk.CastImageFilter()
+    caster.SetOutputPixelType(sitk.sitkFloat32)
+    floatImage = caster.Execute( itkimage )
+    
+    itkimage = sitk.GradientAnisotropicDiffusion(floatImage,
+                      timeStep = timeStep,
+                      conductanceParameter=conductanceParameter,
+                      conductanceScalingUpdateInterval=conductanceScalingUpdateInterval,
+                      numberOfIterations = numberOfIterations
+                      )
                
-    image=sitk.GetArrayFromImage(itkimage).astype(memoryType) 
+    image=sitk.GetArrayFromImage(itkimage).astype(np.uint8) 
     return image         
                
                
+#---------------------------------------------------
+def AnisotropicDiffusionFilter_Curvature(image,
+                          timeStep = 0.0625,
+                          conductanceParameter=3.0,
+                          conductanceScalingUpdateInterval=1,
+                          numberOfIterations = 5 ):
+    """AnisotropicDiffusionFilter : see SimpleITK.CurvatureAnisotropicDiffusion
+    :param image : numpy image
+    :param : timseStep
+    :param : conductanceParameter
+    :param : conductanceScalingUpdateInterval
+    :param : numberOfIterations
+    :return: numpy image after filtering
+    
+    :Example:    
+    import VirtualMaterials as vmat
+    filteredImage = vmat.ImageAnalysis.Filters.AnisotropicDiffusionFilter_Curvature(image,
+                          timeStep = 0.0625,
+                          conductanceParameter=3.0,
+                          conductanceScalingUpdateInterval=1,
+                          numberOfIterations = 5 )
+    """          
+    
+    #https://arxiv.org/pdf/1503.00992.pdf
+    assert(image.min()>=0)
+    itkimage = sitk.GetImageFromArray(image.astype(np.uint8))
+    
+    caster = sitk.CastImageFilter()
+    caster.SetOutputPixelType(sitk.sitkFloat32)
+    floatImage = caster.Execute( itkimage )
+    
+    itkimage = sitk.CurvatureAnisotropicDiffusion(floatImage,
+                      timeStep = timeStep,
+                      conductanceParameter=conductanceParameter,
+                      conductanceScalingUpdateInterval=conductanceScalingUpdateInterval,
+                      numberOfIterations = numberOfIterations
+                      )
                
+    image=sitk.GetArrayFromImage(itkimage).astype(np.uint8) 
+    return image                  
